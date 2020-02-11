5f213a50e9526027ad491d202f098048fe94e1a5
muyun <niu.muyun@gmail.com>
2020-02-11 23:13:12 +0800

    upgrade gradle version

--- document/git.sh
+++ document/git.sh
@@ -1,7 +1,11 @@
 #git commands
+:<<EOF
 git init
 git remote -v
 git remote add origin git@github.com:mu-yun/SpringCloud.git
 git log
 git reset --soft ...
 git restore
+git branch
+git checkout dev
+EOF
---
+++ document/gradle.sh
@@ -0,0 +1,5 @@
+#gradle commands
+:<<EOF
+gradle wrapper --gradle-version 6.1.1
+./gradlew build --scan
+EOF
