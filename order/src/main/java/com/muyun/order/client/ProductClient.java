0e6655a8997a86363667c924e4823676438dbb5b
muyun <niu.muyun@gmail.com>
2020-02-18 10:41:08 +0800

    add request mapping

--- order/src/main/java/com/muyun/order/client/ProductClient.java
+++ order/src/main/java/com/muyun/order/client/ProductClient.java
@@ -3,8 +3,10 @@ package com.muyun.order.client;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @FeignClient("product")
+@RequestMapping("product")
 public interface ProductClient {
 
     @GetMapping("name/{id}")
@@ -12,4 +14,8 @@ public interface ProductClient {
 
     @GetMapping("count/{id}")
     Long getCount(@PathVariable Long id);
+
+    @GetMapping("price/{id}")
+    Long getPrice(@PathVariable Long id);
+
 }
--- order/src/main/java/com/muyun/order/controller/OrderController.java
+++ order/src/main/java/com/muyun/order/controller/OrderController.java
@@ -1,4 +1,21 @@
 package com.muyun.order.controller;
 
+import com.muyun.order.client.ProductClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("order")
 public class OrderController {
+
+    @Autowired
+    private ProductClient productClient;
+
+    @GetMapping("product/{id}")
+    public String getProduct(@PathVariable Long id) {
+        return productClient.getName(id);
+    }
 }
