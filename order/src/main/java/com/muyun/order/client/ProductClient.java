52219db53a28224f2b189e8546ac1c16ec0f98f7
muyun <niu.muyun@gmail.com>
2020-02-18 15:03:32 +0800

    add fallback

--- order/src/main/java/com/muyun/order/client/ProductClient.java
+++ order/src/main/java/com/muyun/order/client/ProductClient.java
@@ -1,21 +1,42 @@
 package com.muyun.order.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient("product")
-@RequestMapping("product")
+//fallback使用@Component的类时，不能使用@RequestMapping
+@FeignClient(name = "product", fallback = ProductClient.ProductClientFallback.class)
 public interface ProductClient {
+    String REQUEST_MAPPING = "product/";
 
-    @GetMapping("name/{id}")
+    @GetMapping(REQUEST_MAPPING + "name/{id}")
     String getName(@PathVariable Long id);
 
-    @GetMapping("count/{id}")
+    @GetMapping(REQUEST_MAPPING + "count/{id}")
     Long getCount(@PathVariable Long id);
 
-    @GetMapping("price/{id}")
+    @GetMapping(REQUEST_MAPPING + "price/{id}")
     Long getPrice(@PathVariable Long id);
 
+    @Component
+    class ProductClientFallback implements ProductClient {
+        @Override
+        public String getName(Long id) {
+            return "fallback-" + id;
+        }
+
+        @Override
+        public Long getCount(Long id) {
+            return null;
+        }
+
+        @Override
+        public Long getPrice(Long id) {
+            return null;
+        }
+    }
+
 }
+
+
