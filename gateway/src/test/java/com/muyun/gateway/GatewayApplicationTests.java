0d9f4c4f4514cd0d5df54670e3090b89bb6543e9
muyun <niu.muyun@gmail.com>
2020-02-25 14:07:02 +0800

    add gateway module

---
+++ gateway/src/test/java/com/muyun/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muyun.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
